f82d10ff3513d9a033d745356e6608d2882924c7
wani1010 <66965971+wani1010@users.noreply.github.com>
2023-12-16 21:33:24 +0530

    Add files via upload
    Add the finished project

---
+++ src/components/App.js
@@ -0,0 +1,152 @@
+import React, { Component } from 'react';
+import DVideo from '../abis/DVideo.json'
+import Navbar from './Navbar'
+import Main from './Main'
+import Web3 from 'web3';
+import './App.css';
+
+//Declare IPFS
+const projectId = '2PL2FUz3S5yR3PUt34o0hJz9tR1';   // <---------- your Infura Project ID
+
+
+const projectSecret = '7b8f5190f4615d5981a13ec8638e5de2';  // <---------- your Infura Secret
+// (for security concerns, consider saving these values in .env files)
+
+const auth = 'Basic ' + new Buffer.from(projectId + ':' + projectSecret).toString('base64');
+
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https', headers: {
+  authorization:auth,
+},
+}); // leaving out the arguments will default to these values
+
+class App extends Component {
+  
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    // Load account
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    // Network ID
+    const networkId = await web3.eth.net.getId()
+    const networkData = DVideo.networks[networkId]
+    if(networkData) {
+      const dvideo = new web3.eth.Contract(DVideo.abi, networkData.address)
+      this.setState({ dvideo })
+      const videosCount = await dvideo.methods.videoCount().call()
+      this.setState({ videosCount })
+      console.log(videosCount)
+      // Load videos, sort by newest
+      for (var i=videosCount; i>=1; i--) {
+        const video = await dvideo.methods.videos(i).call()
+        this.setState({
+          videos: [...this.state.videos, video]
+        })
+      }
+
+      //Set latest video with title to view as default 
+      const latest = await dvideo.methods.videos(videosCount).call()
+      this.setState({
+        currentHash: latest.hash,
+        currentTitle: latest.title
+      })
+      this.setState({ loading: false})
+    } else {
+      window.alert('DVideo contract not deployed to detected network.')
+    }
+  }
+
+  captureFile = event => {
+    event.preventDefault()
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
+
+    reader.onloadend = () => {
+      this.setState({ buffer: Buffer(reader.result) })
+      console.log('buffer', this.state.buffer)
+    }
+  }
+
+
+  uploadVideo = title => {
+    console.log("Submitting file to IPFS...")
+
+    //adding file to the IPFS
+    ipfs.add(this.state.buffer, (error, result) => {
+      console.log('IPFS result', result)
+      if(error) {
+        console.error(error)
+        return
+      }
+
+      this.setState({ loading: true })
+      this.state.dvideo.methods.uploadVideo(result[0].hash, title).send({ from: this.state.account }).on('transactionHash', (hash) => {
+        this.setState({ loading: false })
+      })
+    })
+  }
+
+  changeVideo = (hash, title) => {
+    this.setState({'currentHash': hash});
+    this.setState({'currentTitle': title});
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      buffer: null,
+      account: '',
+      dvideo: null,
+      videos: [],
+      loading: true,
+      currentHash: null,
+      currentTitle: null
+    }
+
+    this.uploadVideo = this.uploadVideo.bind(this)
+    this.captureFile = this.captureFile.bind(this)
+    this.changeVideo = this.changeVideo.bind(this)
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar 
+          account={this.state.account}
+        />
+        { this.state.loading
+          ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
+          : <Main
+              videos={this.state.videos}
+              uploadVideo={this.uploadVideo}
+              captureFile={this.captureFile}
+              changeVideo={this.changeVideo}
+              currentHash={this.state.currentHash}
+              currentTitle={this.state.currentTitle}
+            />
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
